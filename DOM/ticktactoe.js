23cb034d793dba993451b7eb5dcfd55b36fd24a2
Frank Sarnelli <flsarnelli@gmail.com>
2020-02-12 20:51:54 -0600

    dango update

--- DOM/ticktactoe.js
+++ DOM/ticktactoe.js
@@ -1,10 +1,23 @@
-// Reset the game
+var restart = document.querySelector('#restart');
+var squares = document.querySelectorAll('td');
 
+function clearsquares(){
+    for(var i = 0; i < squares.length; i++){
+        squares[i].textContent = '';
+    }
+}
 
-//Grab all of the squares
+function checkSquares(){
+    if(this.textContent === ''){
+        this.textContent = 'X';
+    }else if(this.textContent === 'X'){
+        this.textContent = 'O';
+    }else{
+        this.textContent = ''
+    }
+}
 
-//Clear all the squares
-
-//Check the square marker
-
-// Loop to add event listeners to the squares
+restart.addEventListener('click', clearsquares);
+for(var i = 0; i < squares.length; i++){
+    squares[i].addEventListener('click', checkSquares);
+}
